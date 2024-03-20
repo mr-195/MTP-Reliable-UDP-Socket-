6c0c69ef737860880b11701049cf698c78d3a79d
maverick <manaswiraj195@kgpian.iitkgp.ac.in>
2024-03-20 19:51:55 +0530

    feat: header file created

--- mysock.h
+++ mysock.h
@@ -17,3 +17,80 @@
 #include <string.h>
 #include <stdio.h>
 
+typedef struct
+{
+    short sequence_number;
+    char type;
+    char data[1024];
+    struct sockaddr_in from_addr;
+} recv_packet;
+
+typedef struct
+{
+    struct timeval time;
+    short sequence_number;
+    char type;
+    char data[1024];
+    struct sockaddr_in to_addr;
+} send_packet;
+
+typedef struct
+{
+    int size;
+    int front;
+    int rear;
+    send_packet *window[MAX_WINDOW_SIZE];
+} send_window;
+
+typedef struct
+{
+    int size;
+    int front;
+    int rear;
+    recv_packet *window[MAX_WINDOW_SIZE];
+} recv_window;
+
+typedef struct
+{
+    int size;
+    int front;
+    int rear;
+    send_packet *buffer[MAX_BUFFER_SIZE];
+} send_buff;
+
+typedef struct
+{
+    int size;
+    int front;
+    int rear;
+    recv_packet *buffer[MAX_BUFFER_SIZE];
+} recv_buff;
+
+typedef struct
+{
+    int is_free;
+    pid_t pid;
+    int sockfd;
+    char ip[20];
+    int port;
+    send_window swnd;
+    send_buff sbuff;
+    recv_window rwnd;
+    recv_buff rbuff;
+} shared_memory;
+
+typedef struct
+{
+    int sock_id;
+    char ip[20];
+    int port;
+    int error_no;
+} sock_info;
+// Functions available to application
+
+int m_socket(int domain, int type, int protocol);
+int m_bind(int sockfd, const char *source_ip, int source_port, const char *dest_ip, int dest_port);
+int m_sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
+int m_recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen);
+int m_close(int sockfd);
+int dropMessage(float p);
