85abd5df33b91eae98f117e842361e9941e0bc5b
utsavdhanuka03 <utsavdhanuka03@kgpian.iitkgp.ac.in>
2024-03-20 19:31:43 +0530

    header added

--- mysock.h
+++ mysock.h
@@ -2,3 +2,18 @@
 #define MAX_BUFFER_SIZE 10
 #define MAX_SOCKETS 25
 #define SOCK_MTP 15
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <time.h>
+#include <errno.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
